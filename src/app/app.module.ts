629c60468eaafbe9fdf6be223efec31852de967b
Janusred <56011735+Janusred@users.noreply.github.com>
2020-10-31 04:04:08 -0600

    id y module.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthModule } from '@auth0/auth0-angular';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'janusldsxr-15.us.auth0.com',
+      clientId: '7F0F3db5RjTLqG0IQh3u221sJ6DtNemG'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
